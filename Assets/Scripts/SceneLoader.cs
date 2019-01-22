143000d56df715a7d381767b4b9bf2c1928ae10e
smeerws <hello@smeerws.at>
2019-01-22 01:20:18 +0100

    added sceneloader object + script, + on buttion hook up loadMainScene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
+public class SceneLoader : MonoBehaviour
+{
+    enum Scenes { Welcome, MainScene, GameOver, Won };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Welcome);
+    }
+
+    public void LoadMainGameScene()
+    {
+        SceneManager.LoadScene((int)Scenes.MainScene);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene((int)Scenes.GameOver);
+    }
+
+    public void LoadWonScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Won);
+    }
 }
