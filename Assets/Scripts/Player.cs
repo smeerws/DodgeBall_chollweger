535ee363e2f586382bb89e48aefabfa7fb498e12
smeerws <hello@smeerws.at>
2019-01-22 02:07:14 +0100

    added left/right movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    private const string AXISHORIZONTAL = "Horizontal";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //configruation parameters
+    public float moveSpeed = 5.0f;
+    public float xMin = 1.0f, xMax= 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed;
+        var newPosX = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        transform.position = new Vector2(newPosX, transform.position.y);
+    }
 }
